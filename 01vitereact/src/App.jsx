2c3482a730d1b51fe52e4abccd2664e7c3225ea2
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-10-15 22:15:41 +0530

    made custom react library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Test from './Testfile.jsx'
 function App() {
+  const username = 'htmlCaroline'
 
   return (
     <>
     <h1>React with vite | J</h1>
+    <p>Hello miss {username}</p>
     <Test/>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'> Visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google!'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <App/>
 )
