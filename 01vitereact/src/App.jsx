1854bdf1a7b53a80877dc83c6d0082caa213cdf3
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-10-15 17:28:22 +0530

    first react project done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,11 @@
-
+import Test from './Testfile.jsx'
 function App() {
 
   return (
+    <>
     <h1>React with vite | J</h1>
+    <Test/>
+    </>
   )
 }
 
---
+++ 01vitereact/src/Testfile.jsx
@@ -0,0 +1,6 @@
+function Test(){
+    return(
+        <h4>Test file run successfully</h4>
+    )
+}
+export default Test;
